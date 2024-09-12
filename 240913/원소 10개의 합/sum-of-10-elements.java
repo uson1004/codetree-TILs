9086f5edb1c04ca761eccde3b0557827297f315d
윤유섭 <166105837+uson1004@users.noreply.github.com>
2024-09-13 08:02:04 +0900

    Title: 원소 10개의 합, Time: 162ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240913/원소 10개의 합/sum-of-10-elements.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int val = 0, sum = 0;
+        for(int i = 0; i < 10; i++) {
+            val = sc.nextInt();
+            sum += val;
+        }
+        System.out.print(sum);
+    }
+}
