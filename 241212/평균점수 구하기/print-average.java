7d1e14a6785d3deb0d58b97b37546b223bfdd683
윤유섭 <166105837+uson1004@users.noreply.github.com>
2024-12-12 08:28:41 +0900

    Title: 평균점수 구하기, Time: 138ms, Memory: 13MB, Status: Passed - CodeTree

---
+++ 241212/평균점수 구하기/print-average.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double arr[] = new double[8];
+        double sum = 0;
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextDouble();
+            sum += arr[i];
+        }
+        System.out.printf("%.1f", sum / 8);
+    }
+}
